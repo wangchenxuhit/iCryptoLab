d49980b094c3e93b21282be2f836f7a7e678a4b6
cxwang <hellocxwang@foxmail.com>
2020-05-22 00:42:16 +0800

    busy and out_valid timing modified. format is also modified.

--- 03_sha1/sha1_exec.v
+++ 03_sha1/sha1_exec.v
@@ -61,7 +61,7 @@ module sha1_exec (
 
   //
   // Counts the sha1 rounds.
-  // -- 80 of them (perform 1 rnd/clk)
+  // Total 80 rounds(perform 1 rnd/clk)
   //
   reg    [6:0]   rnd_cnt_d;   
   wire   [6:0]   rnd_cnt_q;  
@@ -100,25 +100,29 @@ module sha1_exec (
   //
   wire [511:0]   w_d;
   wire [511:0]   w_q;
-  wire [511:0]   w_temp;
+  wire [511:0]   w_q_temp;
   wire [31:0]    w;    
 
   // Generate the W words for each round
-  wire [31:0]    w_gen_temp = w_temp[511:480] ^ w_temp[447:416] ^ 
-                 w_temp[255:224] ^ w_temp[95:64];  
+  wire [31:0]    w_gen_temp = w_q_temp[511:480] ^ w_q_temp[447:416] ^
+                 w_q_temp[255:224] ^ w_q_temp[95:64];
   wire [31:0]    w_gen = {w_gen_temp[30:0], w_gen_temp[31]};
   
-  // Select the incoming msg values or the computed ones
-  assign         w_d = ((load_in == 1'b1) ? w_temp :
-                 ((rnd_cnt_q < 7'd15) ? w_temp : 
-                 {w_gen, w_temp[479:0]}));
-
-  // Msg shift register.
+  //
   // Left Circular shift by 1 word 
-  assign w_temp = (cstate == CALC) ? {w_q[479:0], w_q[511:480]} : 
-    ((load_in == 1'b1) ? {w_q[479:0], data_in} : w_q);
+  //
+  assign w_q_temp = (cstate == CALC) ? {w_q[479:0], w_q[511:480]} : w_q;
 
-  // W inputs come straight from a register output
+  //
+  // Select the incoming msg values or the computed ones
+  //
+  assign w_d = (load_in == 1'b1) ? {w_q[479:0], data_in} :
+               (rnd_cnt_q < 7'd15) ? w_q_temp :
+               {w_gen, w_q_temp[479:0]};
+
+  //
+  // w inputs come straight from a register output
+  //
   assign w = w_q[511:480];
   
   sha1_round sha1_round (
@@ -128,104 +132,95 @@ module sha1_exec (
     .rnd_out( sha1_round_wire )
   );
 
+  //
   // Accept the message input
-  assign         rnd_d = ((cstate == CALC) ? sha1_round_wire : 
-    (use_prev_cv == 1'b1) ? cv_next_q : cv);
-  assign         cv_d = ((cstate == CALC) ? cv_q : 
-    (use_prev_cv == 1'b1) ? cv_next_q : cv);
+  //
+  assign rnd_d = (cstate == CALC) ? sha1_round_wire :
+                 (use_prev_cv == 1'b1) ? cv_next_q : cv;
 
+  assign cv_d = (cstate == CALC) ? cv_q :
+                (use_prev_cv == 1'b1) ? cv_next_q : cv;
+
+  //
   // Calculate the final round...
-  assign         cv_next_d = (cstate == VALID_OUT) ? 
-                               ({cv_q[159:128] + rnd_q[159:128],
-                                 cv_q[127:96] + rnd_q[127:96],
-                                 cv_q[95:64] + rnd_q[95:64],
-                                 cv_q[63:32] + rnd_q[63:32],
-                                 cv_q[31:0] + rnd_q[31:0]}) :
-                                 (cv_next_q);
+  //
+  assign cv_next_d = (cstate == VALID_OUT) ? {cv_q[159:128] + rnd_q[159:128],
+                                              cv_q[127:096] + rnd_q[127:096],
+                                              cv_q[095:064] + rnd_q[095:064],
+                                              cv_q[063:032] + rnd_q[063:032],
+                                              cv_q[031:000] + rnd_q[031:000]} :
+                                              cv_next_q;
 
-  // Output the hash
+  //
+  // Output the Hash
+  //
   assign         cv_next = cv_next_q;
 
   //
-  // FSM start at IDLE from reset...
-  // Only thing to note is that start going high restarts the state machine.
+  // FSM start at IDLE from reset
   //
   always @( cstate or start or rnd_cnt_q ) begin
-       out_valid = 1'b0;       
-       busy = 1'b0;
-       rnd_cnt_d = 7'b0000000;
-       case (cstate)
-         IDLE : begin
-            out_valid = 1'b0;
-            rnd_cnt_d = 7'b0000000;
-            if (start) begin
-               busy = 1'b1;
-               nstate = CALC;
-            end
-            else begin
-               busy = 1'b1;
-               nstate = IDLE;
-            end
-         end // case: IDLE
-         
-         CALC : begin
-            busy = 1'b1;
-            out_valid = 1'b0;
-            if (start) begin
-               nstate = IDLE;
-               rnd_cnt_d = 7'b0000000;
-            end
-            else if (rnd_cnt_q == 7'd79) begin
-               nstate = VALID_OUT;
-               rnd_cnt_d = 7'b0000000;
-            end
-            else begin
-               nstate = CALC;
-               rnd_cnt_d = rnd_cnt_q + 7'd0000001;
-            end
-         end // case: CALC
-
-         VALID_OUT : begin
-            busy = 1'b1;
-            out_valid = 1'b0;       
-            // Allow cycle to latch output
-            if (start) begin
-               nstate = IDLE;
-               rnd_cnt_d = 7'b0000000;
-            end else begin
-               nstate = VALID_OUT2;
-            end
-         end     
-
-         VALID_OUT2 : begin
-            busy = 1'b0;
-            out_valid = 1'b1;
-            if (start) begin
-               nstate = CALC;
-            end
-            else begin
-               nstate = IDLE;
-            end
-         end
-
-         default : begin
-            nstate = IDLE;
-         end
-
-       endcase
-    end          
+    out_valid = 1'b0;
+    busy = 1'b0;
+    rnd_cnt_d = 7'b0000000;
+    case ( cstate )
+      IDLE : begin
+        out_valid = 1'b0;
+        rnd_cnt_d = 7'b0000000;
+        if ( start ) begin
+          busy = 1'b1;
+          nstate = CALC;
+        end
+        else begin
+          busy = 1'b0;
+          nstate = IDLE;
+        end
+      end // case : IDLE
+      CALC : begin
+        out_valid = 1'b0;
+        busy = 1'b1;
+        if ( rnd_cnt_q == 7'd79 ) begin
+          nstate = VALID_OUT;
+          rnd_cnt_d = 7'b0000000;
+        end
+        else begin
+          nstate = CALC;
+          rnd_cnt_d = rnd_cnt_q + 7'b0000001;
+        end
+      end // case : CALC
+      //
+      // Allow cycle to latch output
+      VALID_OUT : begin
+        out_valid = 1'b0;
+        busy = 1'b1;
+        nstate = VALID_OUT2; 
+      end // case : VALID_OUT
+      VALID_OUT2 : begin
+        out_valid = 1'b1;
+        busy = 1'b0;
+        nstate = IDLE;
+      end // case : VALID_OUT2
+      default : begin
+        nstate = IDLE;
+      end
+    endcase
+  end          
   
+  //
   // State Elements...
-  dffhr #(7) rnd_cnt_reg (.d(rnd_cnt_d), .q(rnd_cnt_q),
-                          .clk(clk), .r(reset));
-  dffhr #(2) state_reg (.d(nstate), .q(cstate), 
-                        .clk(clk), .r(reset)); 
-  dffhr #(512) w_reg (.d(w_d), .q(w_q), .clk(clk), .r(reset));
-  dffhr #(160) cv_reg (.d(cv_d), .q(cv_q), .clk(clk), .r(reset));
-  dffhr #(160) rnd_reg (.d(rnd_d), .q(rnd_q), .clk(clk), .r(reset));
-  dffhr #(160) cv_next_reg (.d(cv_next_d), .q(cv_next_q),
-                            .clk(clk), .r(reset));   
+  //
+  dffhr #(7)   rnd_cnt_reg (
+    .d(rnd_cnt_d), .q(rnd_cnt_q), .clk(clk), .r(reset));
+  dffhr #(2)   state_reg (
+    .d(nstate), .q(cstate), .clk(clk), .r(reset));
+  dffhr #(512) w_reg (
+    .d(w_d), .q(w_q), .clk(clk), .r(reset));
+  dffhr #(160) cv_reg (
+    .d(cv_d), .q(cv_q), .clk(clk), .r(reset));
+  dffhr #(160) rnd_reg (
+    .d(rnd_d), .q(rnd_q), .clk(clk), .r(reset));
+  dffhr #(160) cv_next_reg (
+    .d(cv_next_d), .q(cv_next_q), .clk(clk), .r(reset));
 
 endmodule // sha1_exec
-
 
--- 03_sha1/sha1_testbench.v
+++ 03_sha1/sha1_testbench.v
@@ -19,237 +19,235 @@
 
 module test;
 
-   reg     clk;
-   reg     start;
-   reg [511:0] y;
-   reg [31:0] data_in;
-   reg load_in;
-   reg [159:0] cv;
-   reg use_prev_cv;
-   wire        busy;
-   wire        out_valid;
-   wire [159:0] cv_next;
-   reg          reset;
+  reg           clk;
+  reg           start;
+  reg   [511:0] y;
+  reg   [31:0]  data_in;
+  reg           load_in;
+  reg   [159:0] cv;
+  reg           use_prev_cv;
+  wire          busy;
+  wire          out_valid;
+  wire  [159:0] cv_next;
+  reg           reset;
 
-   sha1_exec sha1_exec (.clk(clk), .reset(reset), .start(start),
-                        .data_in(data_in), .load_in(load_in),
-                        .cv(cv), .use_prev_cv(use_prev_cv), .busy(busy), 
-                        .out_valid(out_valid), .cv_next(cv_next));
-   integer i;
-   reg [159:0] expected_result;
-   
-   initial
-   begin
-      $dumpfile("dump.vcd");
-      $dumpvars();
-   end
+  sha1_exec dut (
+    .clk        (clk), 
+    .reset      (reset), 
+    .load_in    (load_in),
+    .data_in    (data_in), 
+    .start      (start),
+    .cv         (cv), 
+    .use_prev_cv(use_prev_cv), 
+    .busy       (busy), 
+    .out_valid  (out_valid), 
+    .cv_next    (cv_next)
+  );
 
-   initial
-   begin
-      i = 0;
-      
-      clk = 0;
-      start = 0;
-      // Bits of input 
-      y = 512'b0;
-      cv = 160'h0;
-      data_in = 32'b0;
+  integer       i;
+  reg   [159:0] expected_result;
+  
+  initial begin
+     $dumpfile("dump.vcd");
+     $dumpvars();
+  end
+
+  task inputdata;
+    input   [511:0] y;
+    integer         i;
+    begin
+      for( i=0; i<=15; i=i+1 ) begin
+        data_in <= (y >> 512-((i+1)*32));
+        load_in <= 1'b1;
+        // $display("%8x", data_in);
+        @(posedge clk) ; 
+      end
       load_in = 1'b0;
-      use_prev_cv = 1'b0;
-      reset = 1;
-      expected_result = 160'h0;
-      @(posedge clk);
-      @(posedge clk);
-      @(posedge clk);
-      reset = 0;
-      @(posedge clk);
-      @(negedge clk);
+    end
+  endtask
 
-      $display("Test Begun ...");
-      @(posedge clk);
-      y = {"abc", 8'h80, 416'd0, 61'd3, 3'd0};
-      $display("  input is \"abc\"");
-      expected_result = 160'ha9993e364706816aba3e25717850c26c9cd0d89d;
-      inputdata(y);
-      cv = 160'h67452301EFCDAB8998BADCFE10325476C3D2E1F0;
-      start = 1;
-      @(posedge clk);
-      start = 0;
-      while (busy == 1'd1)
-        begin
-           @(posedge clk);
-        end
-      $display("  result is %8h %8h %8h %8h %8h", cv_next[159:128], 
-                  cv_next[127:96], cv_next[95:64], cv_next[63:32], 
-                  cv_next[31:0]);
-      if (cv_next == expected_result) 
-        $display ("Passed...");
-      else
-        $display ("*** FAILED ***");
-      $display("Test Done ...");       
+  task delayx;
+    input   [31:0]  number;
+  begin
+    repeat( number ) @( posedge clk );
+    #1;
+  end
+  endtask
 
-      @(posedge clk);
-    
-      $display("");
+  initial begin
+    clk = 0;
+  end
+  always #5 clk = ~clk;
 
-      $display("Test Begun ...");
-      @(posedge clk);
-      y = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", 8'h80, 56'd0};
-      $display("  input is \"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq\"");
-      expected_result = 160'h84983e441c3bd26ebaae4aa1f95129e5e54670f1;
-      inputdata(y);
-      cv = 160'h67452301EFCDAB8998BADCFE10325476C3D2E1F0;
-      start = 1;
-      @(posedge clk);
-      start = 0;
-      while (busy == 1'd1)
-        begin
-           @(posedge clk);
-        end
-      @(posedge clk);
-      $display("  intermediate result is %8h %8h %8h %8h %8h", cv_next[159:128], 
-                  cv_next[127:96], cv_next[95:64], cv_next[63:32], 
-                  cv_next[31:0]);
-      y = {448'h0, 61'd56, 3'd0};
+  initial begin
+    i = 0;
+    start = 0;
+    y = 512'b0;
+    cv = 160'h0;
+    data_in = 32'b0;
+    load_in = 1'b0;
+    use_prev_cv = 1'b0;
+    expected_result = 160'h0;
+    reset = 1;
+    delayx(3);
+    reset = 0;
+    delayx(2);
+
+    ////////////////////////////////////////////////////////////////////
+    //
+    // Test 1: Simple Test
+    //
+    $display("");       
+    $display("//");
+    $display("// Test 1: Simple Test Begun ...");
+    $display("//");
+    @( posedge clk );
+    y = {"abc", 8'h80, 416'd0, 64'd24};
+    $display("input is \"abc\"");
+    expected_result = 160'ha9993e36_4706816a_ba3e2571_7850c26c_9cd0d89d;
+    inputdata(y);
+    cv = 160'h67452301_EFCDAB89_98BADCFE_10325476_C3D2E1F0;
+    start = 1;
+    @( posedge clk );
+    start = 0;
+    wait( out_valid );
+    @(posedge clk);
+    $display("result is %8h %8h %8h %8h %8h", cv_next[159:128], 
+              cv_next[127:96], cv_next[95:64], cv_next[63:32], 
+              cv_next[31:0]);
+    if ( cv_next == expected_result ) begin
+      $display ("*** Test 1 Passed ...");
+    end
+    else begin
+      $display ("*** Test 1 Failed ...");
+    end
+    $display("*** Test 1 Done ...");       
+    $display("");       
+    delayx(10);
+
+    ////////////////////////////////////////////////////////////////////
+    //
+    // Test 2: More Complex Test
+    //
+    $display("//");
+    $display("// Test 2: More Complex Test Begun ...");
+    $display("//");
+    y = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", 8'h80, 56'd0};
+    $display("input is \"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq\"");
+    expected_result = 160'h84983e44_1c3bd26e_baae4aa1_f95129e5_e54670f1;
+    inputdata(y);
+    cv = 160'h67452301_EFCDAB89_98BADCFE_10325476_C3D2E1F0;
+    start = 1;
+    @( posedge clk );
+    start = 0;
+    wait( out_valid );
+    @( posedge clk );
+    $display( "intermediate result is %8h %8h %8h %8h %8h", cv_next[159:128], 
+              cv_next[127:96], cv_next[95:64], cv_next[63:32], 
+              cv_next[31:0] );
+    //
+    // Test 2: The Second Block
+    //
+    y = {448'h0, 61'd56, 3'd0};
+    inputdata(y);
+    start = 1;
+    use_prev_cv = 1;
+    @( posedge clk );
+    start = 0;
+    use_prev_cv = 0;
+    wait( out_valid );
+    @( posedge clk );
+    $display( "result is %8h %8h %8h %8h %8h", cv_next[159:128], 
+              cv_next[127:96], cv_next[95:64], cv_next[63:32], 
+              cv_next[31:0] );
+    if ( cv_next == expected_result ) begin
+      $display ("*** Test 2 Passed ...");
+    end
+    else begin
+      $display ("*** Test 2 Failed ...");
+    end
+    $display("*** Test 2 Done ...");       
+    $display("");       
+    delayx(10);
+    $stop;
+
+    ////////////////////////////////////////////////////////////////////
+    //
+    // Test 3: Most Complex Test
+    // Warning!!!  The following test takes ~50 minutes on my machine!!
+    //
+    /*
+    $display("//");
+    $display("// Test 3: Most Complex Test Begun ...");
+    $display("//");
+    use_prev_cv = 1'b0;
+    y = {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+    $display("  input is one million \"a\"s");
+    expected_result = 160'h34aa973c_d4c4daa4_f61eeb2b_dbad2731_6534016f;      
+    inputdata(y);
+    cv = 160'h67452301_EFCDAB89_98BADCFE_10325476_C3D2E1F0;
+    start = 1;
+    @( posedge clk );
+    start = 0;
+    wait( out_valid );
+    @( posedge clk );
+    $display( "result is %8h %8h %8h %8h %8h", cv_next[159:128], 
+              cv_next[127:96], cv_next[95:64], cv_next[63:32], 
+              cv_next[31:0] ); 
+    for ( i=1; i<15625 ; i=i+1 ) begin
       inputdata(y);
       start = 1;
       use_prev_cv = 1;
       @(posedge clk);
       start = 0;
       use_prev_cv = 0;
-      while (busy == 1'd1)
-        begin
-           @(posedge clk);
-        end
-      $display("  result is %8h %8h %8h %8h %8h", cv_next[159:128], 
-                  cv_next[127:96], cv_next[95:64], cv_next[63:32], 
-                  cv_next[31:0]);
-      if (cv_next == expected_result) 
-        $display ("Passed...");
-      else 
-        $display ("*** FAILED ***");
-      $display("Test Done ..."); 
+      wait( out_valid );
+      @( posedge clk );
+      if (i%10 == 0) $display("%d of 15625 Iterations Completed!", i);
+    end
+    //
+    // The last data block for Test 3
+    //
+    y = {8'h80, 440'd0, 61'd1000000, 3'd0};
+    inputdata(y);
+    start = 1;
+    use_prev_cv = 1;
+    @(posedge clk);
+    start = 0;
+    wait( out_valid );
+    @( posedge clk );
+    $display( "result is %8h %8h %8h %8h %8h", cv_next[159:128], 
+              cv_next[127:96], cv_next[95:64], cv_next[63:32], 
+              cv_next[31:0] );
+    if ( cv_next == expected_result ) begin
+      $display ("Test 3 Passed ...");
+    end
+    else begin
+      $display ("Test 3 Failed ...");
+    end
+    $display("Test 3 Done ...");       
+    $display("");       
+    delayx(10);
+    $stop;
+    */
+  end // end of initial
 
-      $display("");
-      @(posedge clk);
-
-      $stop;
-
-      /*
-      ///////////////////////////////////////////////////////////////////////
-      // Warning!!!  The following test takes ~50 minutes on my machine!!
-      ///////////////////////////////////////////////////////////////////////
-      $display("Test Begun ...");
-      @(posedge clk);
-      y = {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
-      $display("  input is one million \"a\"s");
-      expected_result = 160'h34aa973cd4c4daa4f61eeb2bdbad27316534016f;      
-      inputdata(y);
-      cv = 160'h67452301EFCDAB8998BADCFE10325476C3D2E1F0;
-      start = 1;
-      @(posedge clk);
-      start = 0;
-      while (busy == 1'd1)
-        begin
-           @(posedge clk);
-        end
-      @(posedge clk);
-      
-      $display("  ** %8h %8h %8h %8h %8h **", cv_next[159:128], 
-                  cv_next[127:96], cv_next[95:64], cv_next[63:32], 
-                  cv_next[31:0]); 
-      for (i=1; i<15625 ; i=i+1)
-        begin
-          inputdata(y);
-          start = 1;
-          use_prev_cv = 1;
-          @(posedge clk);
-          start = 0;
-          use_prev_cv = 0;
-          while (busy == 1'd1)
-          begin
-            @(posedge clk);
-          end
-          if (i%10 == 0) $display("  %d of 15625 iterations completed", i);
-        end
-      y = {8'h80, 440'd0, 61'd1000000, 3'd0};
-      inputdata(y);
-      start = 1;
-      use_prev_cv = 1;
-      @(posedge clk);
-      start = 0;
-      while (busy == 1'd1)
-        begin
-           @(posedge clk);
-        end
-     
-      $display("  result is %8h %8h %8h %8h %8h", cv_next[159:128], 
-                  cv_next[127:96], cv_next[95:64], cv_next[63:32], 
-                  cv_next[31:0]);
-      if (cv_next == expected_result) 
-        $display ("Passed...");
-      else 
-        $display ("*** FAILED ***");
-      $display("Test Done ..."); 
-
-      */
-      @(posedge clk);
-      @(posedge clk);
-      @(posedge clk);
-      @(posedge clk);
-
-      $stop;
-
-   end // end of initial
-
-
-  /////////////////////////////////////////////////////////
-  // Create System Clock
-  always #5 clk = ~clk;
-
-  /////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////
+  //
   // Debugging printouts of all pertinent state...
+  //
   /*
-  always@(posedge clk)
-  begin
-    if (sha1_exec.state == 2'b01)
-      $display("%8h %8h %8h %8h %8h -- %8h %8h %8h-- round: %d", 
+  always @( posedge clk ) begin
+    if (sha1_exec.state == 2'b01) begin
+      $display( "%8h %8h %8h %8h %8h -- %8h %8h %8h-- round: %d", 
         sha1_exec.sha1_round.a, sha1_exec.sha1_round.b, 
         sha1_exec.sha1_round.c, sha1_exec.sha1_round.d,
         sha1_exec.sha1_round.e, sha1_exec.sha1_round.w, 
         sha1_exec.sha1_round.f, sha1_exec.sha1_round.k, 
-        sha1_exec.rnd_cnt_q);
-  end
-  */
-
-  ////////////////////////////////////////////////////////////////////////
-  // Dump VCD file
-  ///////////////////////////////////////////////////////////////////////
-  /*
-  initial
-    begin
-       $vcdpluson(0,test);
+        sha1_exec.rnd_cnt_q );
     end
-  */
-
-
-task inputdata;
-  input [511:0] y;
-  integer i;
-
-  begin
-    for (i=0; i<=15; i=i+1) 
-      begin
-        data_in <= (y >> 512-((i+1)*32));
-        load_in <= 1'b1;
-        // $display("%8x", data_in);
-        @(posedge clk) ; 
-      end
-    load_in = 1'b0;
   end
-endtask
+  */
 
 endmodule // test
-
-
 
